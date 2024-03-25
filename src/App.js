c8ff8b128c9136553eac6f35e6b3a6d086ff58ad
RaviTesla1 <ravibatalvi55@gmail.com>
2024-03-25 20:34:04 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [loginData,setLoginData] = useState({
+    userName:"",
+    password:""
+  })
+  const [error,setError] = useState({
+    userName:"",
+    password:""
+  })
+  // const [Error, setError] = useState("");
+
+  const handleChange = (e) => {
+    const {name,value} = e.target;
+    setLoginData({
+      ...loginData,
+      [name] : value
+    })
+    // e.target.value.length <= 4 ? setError("Email Required") : setError("");
+    console.warn(name,value)
+  };
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setError(prevState => ({
+      ...prevState,
+      userName: loginData.userName.length <= 4 ? "Error" : "",
+      password: loginData.password.length <= 4 ? "Error" : ""
+    }));
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form style={{ margin: "50px" }} onSubmit={handleSubmit}>
+      <input
+       type="text"
+        name="userName"
+        placeholder="Username"
+        value={loginData.userName}
+        onChange={handleChange}
+      />
+      <p style={{ color: "red" }}>{error.userName}</p>
+      <input
+        type="password"
+        name="password"
+        placeholder="Password"
+        value={loginData.password}
+        onChange={handleChange}
+        style={{ marginLeft: "10px" }}
+      />
+            <p style={{ color: "red" }}>{error.password}</p>
+      <button type="submit" style={{ marginLeft: "10px" }}>
+        Submit
+      </button>
+    </form>
   );
 }
 
